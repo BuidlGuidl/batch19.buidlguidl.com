408c1b2fff1e06185a3f559c0c4f90aa4c88261a
Ethan Rouimi <contact@ethan-rouimi.com>
2025-08-19 13:53:16 +0200

    eliminate duplicates in builders

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -1,15 +1,18 @@
-"use client";
-
-import type { NextPage } from "next";
+import fs from "fs/promises";
+import path from "path";
 import { BuildersList } from "~~/components/builders/BuildersList";
 
-const BuildersPage: NextPage = () => {
+async function BuildersPage() {
+  const buildersPath = path.join(process.cwd(), "app", "builders");
+  const files = await fs.readdir(buildersPath, { withFileTypes: true });
+  const buildersPages = files.filter(file => file.isDirectory()).map(dir => dir.name);
+
   return (
     <div className="flex justify-center align-middle flex-col ">
-      <h1 className="text-center my-5 text-xl">Builders</h1>
-      <BuildersList />
+      <h1 className="text-center my-5 text-xl">Batch 19 Builders</h1>
+      <BuildersList buildersPages={buildersPages} />
     </div>
   );
-};
+}
 
 export default BuildersPage;
--- packages/nextjs/components/builders/BuildersList.tsx
+++ packages/nextjs/components/builders/BuildersList.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-// import { getBuilderPages } from '~~/utils/getBuilderFolders';
+import Link from "next/link";
 import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
-export function BuildersList() {
+interface BuildersListProps {
+  buildersPages: string[];
+}
+
+export function BuildersList({ buildersPages }: BuildersListProps) {
   const {
     data: events,
     isLoading: isLoading,
-    // error: errorReadingEvents,
+    error: errorReadingEvents,
   } = useScaffoldEventHistory({
     contractName: "BatchRegistry",
     eventName: "CheckedIn",
@@ -17,20 +21,33 @@ export function BuildersList() {
     blockData: false,
     transactionData: false,
     receiptData: false,
+    blocksBatchSize: 500_000,
   });
 
-  return isLoading ? (
+  const uniqueEvents = events?.filter(event => event?.args?.first);
+
+  return errorReadingEvents !== null ? (
+    <p>An error ocurred processing the builders</p>
+  ) : isLoading ? (
     <p>Loading checked-in events...</p>
   ) : (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 gap-2 justify-items-center px-2 w-max self-center">
-      {events.map((event, index) => {
+      {uniqueEvents.map((event, index) => {
         return (
           <div key={index} className="card w-96 bg-base-100 card-sm shadow-sm">
             <div className="card-body">
               <Address address={event.args.builder} />
               <div className="justify-end card-actions">
-                <button className="btn btn-primary">Contract</button>
-                <button className="btn btn-primary">Page</button>
+                {buildersPages.includes(event.args.builder ?? "") ? (
+                  <>
+                    <Link className="btn btn-secondary btn-sm" href={`/builders/${event.args.builder}`}>
+                      Page
+                    </Link>
+                    <button className="btn btn-secondary btn-sm">Contract</button>
+                  </>
+                ) : (
+                  <button className="btn btn-secondary btn-sm">Contract</button>
+                )}
               </div>
             </div>
           </div>
